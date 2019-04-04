0a2dc608339d43d83b01b45cc8ebc18d594df083
TinyKenny <Jonathan@rawet.se>
2019-04-04 11:41:33 +0200

    creating the project

---
+++ SPM_VT19_grupp8/Assets/Scripts/placeHolderScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placeHolderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Why are you using the placeholder?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("WHY ARE YOU USING THE PLACEHOLDER?");
+    }
+}
