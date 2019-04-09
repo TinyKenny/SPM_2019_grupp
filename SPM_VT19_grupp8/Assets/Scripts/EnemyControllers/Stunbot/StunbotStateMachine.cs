a115edf210146d65cbac87e305c48d25d24d2366
TinyKenny <Jonathan@rawet.se>
2019-04-09 18:38:19 +0200

    Enemy!

---
+++ SPM_VT19_grupp8/Assets/Scripts/EnemyControllers/Stunbot/StunbotStateMachine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunbotStateMachine : StateMachine
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+
+    }
+}
