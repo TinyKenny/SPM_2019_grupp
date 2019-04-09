a6d2727a6ffdd4c6da4f650a18a654a170e99c2f
TinyKenny <Jonathan@rawet.se>
2019-04-09 15:53:01 +0200

    projectile start

--- SPM_VT19_grupp8/Assets/Scripts/ProjectileBehaviour.cs
+++ SPM_VT19_grupp8/Assets/Scripts/ProjectileBehaviour.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class ProjectileBehaviour : MonoBehaviour
 {
-    public float speed;
+    public float speed = 15.0f;
+    //public LayerMask;
+
+    private SphereCollider thisCollider;
+
+    public void SetInitialValues()
+    {
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        thisCollider = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = transform.forward * speed * Time.deltaTime;
+
+        //Physics.SphereCast()
+
+        //bool hitSomething = Physics.SphereCast(transform.position, thisCollider.radius, transform.forward, speed * Time.deltaTime, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore);
+
+        transform.position += movement;
     }
 }
