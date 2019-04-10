d0ee06feb3d682eab12fb2b140fce73ef809abbd
TinyKenny <Jonathan@rawet.se>
2019-04-10 11:48:41 +0200

    the rest of stunbot...

--- SPM_VT19_grupp8/Assets/Scripts/EnemyControllers/Stunbot/States/StunbotBaseState.cs
+++ SPM_VT19_grupp8/Assets/Scripts/EnemyControllers/Stunbot/States/StunbotBaseState.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public class StunbotBaseState : State
 {
+    protected Transform PlayerTransform { get { return owner.playerTransform; } }
+    protected float Acceleration { get { return owner.Acceleration; } }
+    protected float Deceleration { get { return owner.Deceleration; } }
+    protected float MaxSpeed { get { return owner.MaxSpeed; } }
+    protected float AirResistanceCoefficient { get { return owner.AirResistanceCoefficient; } }
+    protected float SkinWidth { get { return owner.SkinWidth; } }
+    protected Vector3 Velocity { get { return owner.Velocity; } set { owner.Velocity = value; } }
+    protected SphereCollider ThisCollider { get { return owner.thisCollider; } }
+    protected Transform ThisTransform { get { return owner.transform; } }
 
-     
+    protected StunbotStateMachine owner;
 
+    public override void Initialize(StateMachine owner)
+    {
+        this.owner = (StunbotStateMachine)owner;
+    }
+
+    public override void HandleUpdate()
+    {
+        base.HandleUpdate();
+        Velocity *= Mathf.Pow(AirResistanceCoefficient, Time.deltaTime);
+    }
+
+    protected bool CanSeePlayer()
+    {
+        return !Physics.Linecast(owner.transform.position, PlayerTransform.position, owner.visionMask);
+    }
 }
--- SPM_VT19_grupp8/Assets/Scripts/EnemyControllers/Stunbot/StunbotStateMachine.cs
+++ SPM_VT19_grupp8/Assets/Scripts/EnemyControllers/Stunbot/StunbotStateMachine.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class StunbotStateMachine : StateMachine
 {
+    public Transform playerTransform;
+    public LayerMask visionMask;
+    public LayerMask playerLayer;
+    public SphereCollider thisCollider;
 
+    private PhysicsComponent physicsComponent;
 
+    public float Acceleration { get { return physicsComponent.acceleration; } }
+    public float Deceleration { get { return physicsComponent.deceleration; } }
+    public float MaxSpeed { get { return physicsComponent.maxSpeed; } }
+    public float AirResistanceCoefficient { get { return physicsComponent.airResistanceCoefficient; } }
+    public float SkinWidth { get { return physicsComponent.skinWidth; } }
+    public Vector3 Velocity { get { return physicsComponent.velocity; } set { physicsComponent.velocity = value; } }
+    
 
     protected override void Awake()
     {
         base.Awake();
-
+        physicsComponent = GetComponent<PhysicsComponent>();
+        thisCollider = GetComponent<SphereCollider>();
     }
 
     protected override void Update()
     {
         base.Update();
-
-
+        if (Velocity.magnitude > MathHelper.floatEpsilon)
+        {
+            transform.LookAt(transform.position + Velocity.normalized);
+        }
     }
 }
