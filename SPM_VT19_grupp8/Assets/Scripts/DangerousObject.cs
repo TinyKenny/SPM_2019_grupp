e26fd6de39e06dde2279b77473f626674b462571
victardius <37775040+victardius@users.noreply.github.com>
2019-04-11 19:49:42 +0200

    Update DangerousObject.cs

--- SPM_VT19_grupp8/Assets/Scripts/DangerousObject.cs
+++ SPM_VT19_grupp8/Assets/Scripts/DangerousObject.cs
@@ -6,8 +6,10 @@ public class DangerousObject : MonoBehaviour
 {
     [SerializeField] private float damage;
     [SerializeField] private float knockback;
+    [SerializeField] private float cooldownAmount = 1;
     private PlayerStateMachine player;
     private BoxCollider coll;
+    private float cooldown = 0;
 
     private void Awake()
     {
@@ -17,10 +19,13 @@ public class DangerousObject : MonoBehaviour
 
     private void Update()
     {
+        cooldown -= Time.deltaTime;
         RaycastHit hit;
-        if (Physics.BoxCast(transform.position, coll.bounds.extents, (player.transform.position - transform.position), out hit, transform.rotation, Mathf.Infinity, player.gameObject.layer))
+        if (Physics.BoxCast(transform.position, coll.bounds.extents, (player.transform.position - transform.position).normalized, out hit, Quaternion.identity, player.skinWidth * 10, 1 << 10, QueryTriggerInteraction.Ignore) && cooldown < 0)
         {
+            cooldown = cooldownAmount;
             player.TakeDamage(damage);
+            player.Velocity += (player.transform.position - transform.position).normalized * knockback * player.getPlayerDeltaTime();
         }
     }
 }
