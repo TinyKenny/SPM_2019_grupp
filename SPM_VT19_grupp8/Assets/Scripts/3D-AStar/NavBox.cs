36595a799a5fe4515b7f69156eadda35a640a8e0
victardius <37775040+victardius@users.noreply.github.com>
2019-04-13 23:04:54 +0200

    fixed navmeshbox neighbhours

---
+++ SPM_VT19_grupp8/Assets/Scripts/3D-AStar/NavBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavBox : MonoBehaviour
+{
+    public List<GameObject> neighbours = new List<GameObject>();
+
+    private void Awake()
+    {
+        foreach (GameObject g in neighbours)
+            Debug.Log(g.GetType());
+    }
+}
--- SPM_VT19_grupp8/Assets/Scripts/3D-AStar/NavmeshRenderer.cs
+++ SPM_VT19_grupp8/Assets/Scripts/3D-AStar/NavmeshRenderer.cs
@@ -7,6 +7,8 @@ public class NavmeshRenderer : MonoBehaviour
     [SerializeField] private BoxCollider renderArea;
     [SerializeField] private int precision = 2;
     [SerializeField] private LayerMask colliders;
+    private LayerMask navColl = 1 << 14;
+    private List<BoxCollider> objects;
 
     [ContextMenu("Render a 3D-Navigational mesh for stunbot.")]
     public void Generate3DNavmesh()
@@ -14,7 +16,15 @@ public class NavmeshRenderer : MonoBehaviour
         BoxCollider area = (BoxCollider)GameObject.CreatePrimitive(PrimitiveType.Cube).GetComponent<BoxCollider>();
         area.size = renderArea.size;
         area.center = renderArea.transform.position + renderArea.center;
+        area.gameObject.AddComponent<NavBox>();
+        area.gameObject.layer = 14;
         CheckCollision(area, precision);
+
+        foreach (BoxCollider b in objects)
+        {
+            if (b != null && b.transform.GetComponent<NavBox>() != null)
+                AddNeighbours(b);
+        }
     }
 
     private BoxCollider CheckCollision(BoxCollider area, int recursion)
@@ -30,9 +40,15 @@ public class NavmeshRenderer : MonoBehaviour
                     BoxCollider traversableBox = (BoxCollider)GameObject.CreatePrimitive(PrimitiveType.Cube).GetComponent<BoxCollider>();
                     traversableBox.size = area.size / 2;
                     traversableBox.center = area.transform.position + boxPlacing[i];
+                    DestroyImmediate(traversableBox.GetComponent<MeshRenderer>());
+                    DestroyImmediate(traversableBox.GetComponent<MeshFilter>());
+                    traversableBox.gameObject.AddComponent<NavBox>();
+                    traversableBox.gameObject.layer = 14;
+                    objects.Add(traversableBox);
                     traversableBox = CheckCollision(traversableBox, recursion - 1);
                 }
             }
+            objects.Remove(area);
             DestroyImmediate(area.gameObject);
         }
         else
@@ -40,5 +56,28 @@ public class NavmeshRenderer : MonoBehaviour
             
         }
         return area;
+    }
+
+    private void AddNeighbours(BoxCollider traversableBox)
+    {
+        RaycastHit[] hitsRight = Physics.BoxCastAll(traversableBox.center, traversableBox.size / 2, Vector3.right, Quaternion.identity, MathHelper.floatEpsilon, navColl);
+        //RaycastHit[] hitsLeft = Physics.BoxCastAll(traversableBox.center, traversableBox.size / 2, Vector3.left, Quaternion.identity, MathHelper.floatEpsilon, navColl);
+        //RaycastHit[] hitsUp = Physics.BoxCastAll(traversableBox.center, traversableBox.size / 2, Vector3.up, Quaternion.identity, MathHelper.floatEpsilon, navColl);
+        //RaycastHit[] hitsDown = Physics.BoxCastAll(traversableBox.center, traversableBox.size / 2, Vector3.down, Quaternion.identity, MathHelper.floatEpsilon, navColl);
+        //RaycastHit[] hits = new RaycastHit[hitsRight.Length + hitsLeft.Length + hitsUp.Length + hitsDown.Length];
+        //int pos = 0;
+        //hitsRight.CopyTo(hits, pos);
+        //pos += hitsRight.Length;
+        //hitsLeft.CopyTo(hits, pos);
+        //pos += hitsLeft.Length;
+        //hitsUp.CopyTo(hits, pos);
+        //pos += hitsUp.Length;
+        //hitsDown.CopyTo(hits, pos);
+        //pos += hitsDown.Length;
+        foreach (RaycastHit hit in hitsRight)
+        {
+            if (!hit.transform.gameObject.Equals(traversableBox.gameObject))
+                traversableBox.GetComponent<NavBox>().neighbours.Add(hit.transform.gameObject);
+        }
     }
 }
