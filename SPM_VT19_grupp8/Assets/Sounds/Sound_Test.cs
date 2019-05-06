ce82682d59d428ffe2b6b8c2b9880c11496d057e
Dandarue <37660410+Dandarue@users.noreply.github.com>
2019-05-06 14:50:56 +0200

    Ljud för player

---
+++ SPM_VT19_grupp8/Assets/Sounds/Sound_Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        
+    }
+}
