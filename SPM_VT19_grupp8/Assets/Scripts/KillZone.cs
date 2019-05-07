56a2823331d996cfe3cf04cb524245efcf52b403
Dandarue <37660410+Dandarue@users.noreply.github.com>
2019-05-07 17:15:48 +0200

    asds

--- SPM_VT19_grupp8/Assets/Scripts/KillZone.cs
+++ SPM_VT19_grupp8/Assets/Scripts/KillZone.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class KillZone : MonoBehaviour
 {
-    public AudioSource ausKillZone;
-    public AudioClip killZoneSound;
     private void OnTriggerEnter(Collider other)
     {
         PlayerStateMachine player = other.GetComponent<PlayerStateMachine>();
-            ausKillZone.PlayOneShot(killZoneSound);
         if (player != null)
         {
 
