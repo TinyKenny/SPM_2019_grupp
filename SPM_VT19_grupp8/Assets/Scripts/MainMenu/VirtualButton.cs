f098657dc51a7b9a285d70733f859f90ba9760cd
victardius <37775040+victardius@users.noreply.github.com>
2019-05-08 14:31:42 +0200

    Virtual keyboard added

---
+++ SPM_VT19_grupp8/Assets/Scripts/MainMenu/VirtualButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualButton : MonoBehaviour
+{
+    public InputField input;
+
+    public void WriteLetter()
+    {
+        string letter = GetComponentInChildren<Text>().text;
+        if (letter == "Space")
+        {
+            letter = " ";
+        }
+        if (letter == "<-" && input.text.Length > 0)
+        {
+            input.text = input.text.Substring(0, input.text.Length - 1);
+        }
+        input.text += letter;
+    }
+}
---
+++ SPM_VT19_grupp8/Assets/Scripts/MainMenu/VirtualKeyboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualKeyboard : MonoBehaviour
+{
+    public InputField input;
+
+    void Start()
+    {
+        foreach (Button b in GetComponentsInChildren<Button>())
+        {
+            b.onClick.AddListener(b.GetComponent<VirtualButton>().WriteLetter);
+            b.GetComponent<VirtualButton>().input = input;
+        }
+    }
+}
