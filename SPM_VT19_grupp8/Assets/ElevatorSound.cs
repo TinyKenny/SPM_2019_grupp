a8407cd5d7359b49437bb3c82dd5e38eef89e1b2
Hannovia <33325512+Hannovia@users.noreply.github.com>
2019-05-12 16:04:55 +0200

    Gjorde om lite i första rummet på level 1.5

---
+++ SPM_VT19_grupp8/Assets/ElevatorSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorSound : MonoBehaviour
+{
+
+    private AudioSource source;
+    public float delay;
+
+    // public AudioClip clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource source = GetComponent<AudioSource>();
+        source.PlayDelayed(delay);
+        // source.PlayOneShot(clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
