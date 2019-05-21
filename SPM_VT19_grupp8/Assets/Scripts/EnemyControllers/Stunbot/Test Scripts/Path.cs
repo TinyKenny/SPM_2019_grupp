5bbf3eed6bc2c811996c134e69c20b2383dd0a76
TinyKenny <Jonathan@rawet.se>
2019-05-21 14:58:29 +0200

    adding a bunch of scripts related to path smoothing
    most of these will probably be removed later

---
+++ SPM_VT19_grupp8/Assets/Scripts/EnemyControllers/Stunbot/Test Scripts/Path.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path
+{
+    public readonly Vector3[] lookPoints;
+    public readonly TurnBoundaryPlane[] turnBoundaries;
+    public readonly int finishLineIndex;
+    public readonly int slowDownIndex;
+
+    public Path(Vector3[] waypoints, Vector3 startPosition, float turnDst, float stoppingDst)
+    {
+        lookPoints = waypoints;
+        turnBoundaries = new TurnBoundaryPlane[lookPoints.Length];
+        finishLineIndex = turnBoundaries.Length - 1;
+
+        Vector3 previousPoint = startPosition;
+
+        for(int i = 0; i < lookPoints.Length; i++)
+        {
+            Vector3 currentPoint = lookPoints[i];
+            Vector3 dirToCurrentPoint = (currentPoint - previousPoint).normalized;
+            Vector3 turnBoundaryPoint = (i == finishLineIndex) ? currentPoint : currentPoint - dirToCurrentPoint * turnDst;
+
+            //this might be completely wrong...
+            turnBoundaries[i] = new TurnBoundaryPlane(turnBoundaryPoint, previousPoint - dirToCurrentPoint * turnDst);
+
+            previousPoint = turnBoundaryPoint;
+        }
+
+        float dstFromEndpoint = 0;
+        for(int i = lookPoints.Length - 1; i > 0; i--)
+        {
+            dstFromEndpoint += Vector3.Distance(lookPoints[i], lookPoints[i - 1]);
+            if(dstFromEndpoint > stoppingDst)
+            {
+                slowDownIndex = i;
+                break;
+            }
+        }
+    }
+}
---
+++ SPM_VT19_grupp8/Assets/Scripts/EnemyControllers/Stunbot/Test Scripts/TurnBoundaryPlane.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct TurnBoundaryPlane
+{
+    Plane plane;
+    bool approachSide;
+
+    public TurnBoundaryPlane(Vector3 pointOnPlane, Vector3 pointPerpendicularToPlane)
+    {
+        plane = new Plane((pointPerpendicularToPlane - pointOnPlane).normalized, pointOnPlane);
+
+        approachSide = false;
+        approachSide = plane.GetSide(pointPerpendicularToPlane);
+    }
+
+    public bool HasCrossedLine(Vector3 p)
+    {
+        return plane.GetSide(p) != approachSide;
+    }
+
+    public float DistanceFromPoint(Vector3 p)
+    {
+        return plane.GetDistanceToPoint(p);
+    }
+}
