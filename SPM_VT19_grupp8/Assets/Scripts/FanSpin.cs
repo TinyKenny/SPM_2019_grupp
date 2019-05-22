01d79da2c05014887d15711369065d772f76d593
CthulhuTheKing <37659836+CthulhuTheKing@users.noreply.github.com>
2019-05-22 12:23:28 +0200

    Level all And fan script

---
+++ SPM_VT19_grupp8/Assets/Scripts/FanSpin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanSpin : MonoBehaviour
+{
+  public  float speed = 10;
+
+
+    void Start()
+    {
+       
+  
+    }
+
+    void Update()
+    {
+
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        
+    
+    }
+}
