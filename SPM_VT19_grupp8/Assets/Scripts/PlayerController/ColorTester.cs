c4f99b638cd8ee0eb49668a9e56b2e79d0ee965e
TinyKenny <Jonathan@rawet.se>
2019-05-27 13:51:37 +0200

    adding color-tester script

---
+++ SPM_VT19_grupp8/Assets/Scripts/PlayerController/ColorTester.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTester : MonoBehaviour
+{
+    [SerializeField] private Color32 testColor = Color.white;
+    private Renderer rend;
+
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        rend.material.color = testColor;
+    }
+}
