45442e1c736938562ea3ddfd4f020860aac83063
TinyKenny <Jonathan@rawet.se>
2019-06-03 14:59:50 +0200

    lite kod-kvalite-grejer

--- SPM_VT19_grupp8/Assets/Scripts/PhysicsComponent.cs
+++ SPM_VT19_grupp8/Assets/Scripts/PhysicsComponent.cs
@@ -8,18 +8,15 @@ public class PhysicsComponent : MonoBehaviour
     public float Acceleration { get { return acceleration; } }
     public float Deceleration { get { return deceleration; } }
     public float MaxSpeed { get { return maxSpeed; } }
-    public float FrictionCoefficient { get { return frictionCoefficient; } }
     public float AirResistanceCoefficient { get { return airResistanceCoefficient; } }
     public float Gravity { get { return gravity; } }
     public float SkinWidth { get { return skinWidth; } }
     public Vector3 Velocity { get; set; } = new Vector3(0.0f, 0.0f, 0.0f);
 
-    [SerializeField] private float acceleration;
-    [SerializeField] private float deceleration;
-    [SerializeField] private float maxSpeed;
-    [SerializeField] private float frictionCoefficient = 0.4f;
-    [Range(0.0f, 1.0f)]
-    [SerializeField] private float airResistanceCoefficient = 0.95f;
-    [SerializeField] private float gravity;
-    [SerializeField] private float skinWidth = 0.01f;
+    [SerializeField, Min(0.0f)] private float acceleration;
+    [SerializeField, Min(0.0f)] private float deceleration;
+    [SerializeField, Min(0.0f)] private float maxSpeed;
+    [SerializeField, Range(0.0f, 1.0f)] private float airResistanceCoefficient = 0.95f;
+    [SerializeField, Min(0.0f)] private float gravity;
+    [SerializeField, Min(0.0f)] private float skinWidth = 0.01f;
 }
