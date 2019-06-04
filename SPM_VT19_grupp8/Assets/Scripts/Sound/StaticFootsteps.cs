9968fa86834e68453a2330d7b9759c533a26815a
Dandarue <37660410+Dandarue@users.noreply.github.com>
2019-06-04 13:45:25 +0200

    footsteps

---
+++ SPM_VT19_grupp8/Assets/Scripts/Sound/StaticFootsteps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footstep : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PlayStaticFootsteps()
+    {
+        source.PlayOneShot(clip);
+    } 
+
+}
